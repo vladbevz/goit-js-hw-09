920f6e4cc4b2accd321710d42774d09c58ef28fa
vladbevz <bevz.vlad15@gmail.com>
2023-07-10 22:36:54 +0200

    update task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+const formEl = document.querySelector('.form');
+let delayInp = null;
+let stepInp = null;
+let amountInp = null;
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function submitHandler(e) {
+  e.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+  delayInp = parseInt(delay.value);
+  stepInp = parseInt(step.value);
+  amountInp = parseInt(amount.value);
+
+  for (let i = 0; i < amountInp; i++) {
+    createPromise(i, delayInp)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+
+    delayInp += stepInp;
   }
 }
+
+formEl.addEventListener('submit', submitHandler);
