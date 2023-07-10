e3538cff6c63d7722ea88dde94559cc2ae7e6ea6
vladbevz <bevz.vlad15@gmail.com>
2023-07-10 22:21:07 +0200

    update task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let bodyColor = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
+
+buttonStart.addEventListener('click', () => {
+  bodyColor = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true;
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(bodyColor);
+  buttonStart.disabled = false;
+});
