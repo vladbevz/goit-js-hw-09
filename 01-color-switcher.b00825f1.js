cbbfa7c27875d9aada2c3bf7c5ae2f6dfebfedf5
vladbevz <122693446+vladbevz@users.noreply.github.com>
2023-07-10 21:25:47 +0000

    Deploying to gh-pages from @ vladbevz/goit-js-hw-09@f2f6692516038cbf2ca2476200e15b5609959bf3 🚀

---
+++ 01-color-switcher.b00825f1.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.b00825f1.js.map
